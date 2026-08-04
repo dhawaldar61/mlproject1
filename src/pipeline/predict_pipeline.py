6ba37550080d0be49f7df42237a97d4beda7defd
dhawaldar61 <Dhirajhawaldar61@gmail.com>
2026-08-04 08:54:06 +0530

    app.py done

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pandas as pd
 from src.exception import CustomException
@@ -7,21 +8,23 @@ class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self,features):
+    def predict(self, features):
         try:
-            model_path='artifacts/model.pkl'
-            preprocessor_path='artifacts/preprocessor.pkl'
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
+            base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+            model_path = os.path.join(base_dir, 'artifacts', 'model.pkl')
+            preprocessor_path = os.path.join(base_dir, 'artifacts', 'preprocessor.pkl')
+
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
             return preds
         except Exception as e:
             raise CustomException(e, sys)
-        
+
 
 class CustomData:
-    def __init__(self,gender: str,
+    def __init__(self, gender: str,
         race_ethnicity: str,
         parental_level_of_education,
         lunch: str,
@@ -29,19 +32,12 @@ class CustomData:
         reading_score: int,
         writing_score: int):
 
-
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
@@ -57,6 +53,6 @@ class CustomData:
             }
 
             return pd.DataFrame(custom_data_input_dict)
-        
+
         except Exception as e:
             raise CustomException(e, sys)
